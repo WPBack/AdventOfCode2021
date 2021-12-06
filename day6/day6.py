9e3d2eb88247007dd12c0afa30bd9ef9835cd145
WPBack <daniel.a.back@telia.com>
2021-12-06 19:03:41 +0100

    Solved day 6 part 1

---
+++ day6/day6.py
@@ -0,0 +1,63 @@
+# Import modules
+import os
+from termcolor import colored
+
+# Input parser
+def input_parser(filename):
+    # Open the file
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    inputFile = open(os.path.join(__location__, filename), 'r')
+
+    # Process the file
+    input = [int(i) for i in inputFile.readline().split(',')]
+
+    # Close the file and return the result
+    inputFile.close()
+    return input
+
+# Puzzle 1
+def puzzle1(filename):
+    # Read file
+    fishes = input_parser(filename)
+
+    # Simulate over 80 days
+    for day in range(80):
+        numNewFish = 0
+        for i in range(len(fishes)):
+            if fishes[i] == 0:
+                fishes[i] = 6
+                numNewFish += 1
+            else:
+                fishes[i] -= 1
+        fishes.extend([8]*numNewFish)
+
+    return len(fishes)
+
+# Puzzle 2
+def puzzle2(filename):
+    # Read file
+    input = input_parser(filename)
+
+    return 0
+
+# Run tests for puzzle 1
+puzzle1TestPass = puzzle1('example1') == 5934
+if(puzzle1TestPass):
+    print(colored('Tests for puzzle 1 PASS', 'green'))
+else:
+    print(colored('Tests for puzzle 1 FAIL', 'red'))
+
+# Solve puzzle 1 if test passed
+if(puzzle1TestPass):
+    print('Solution for puzzle 1: ' + str(puzzle1('input')))
+
+# Run tests for puzzle 2
+puzzle2TestPass = puzzle2('example1') == 2
+if(puzzle2TestPass):
+    print(colored('Tests for puzzle 2 PASS', 'green'))
+else:
+    print(colored('Tests for puzzle 2 FAIL', 'red'))
+
+# Solve puzzle 2 if test passed
+if(puzzle2TestPass):
+    print('Solution for puzzle 2: ' + str(puzzle2('input')))
