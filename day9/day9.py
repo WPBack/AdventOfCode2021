d913d217929973d754f980af9d5866131a46021d
WPBack <daniel.a.back@telia.com>
2021-12-11 16:08:36 +0100

    Solved day 9 part 2

--- day9/day9.py
+++ day9/day9.py
@@ -42,11 +42,58 @@ def puzzle1(filename):
     return result
 
 # Puzzle 2
+# Recursive function that finds the size of a basin given the map, a point and the previous point (to not count that again)
+def getBasinSize(map, checkedMap, row, col):
+    checkedMap[row][col] = True
+    # Check if we have a high point
+    if map[row][col] == 9:
+        return 0
+    else:
+        basinSize = 1
+        # TODO: This probably must be replaced by a boolean map that keeps track on if a point is already included
+        if row > 0 and not checkedMap[row-1][col]:
+            basinSize += getBasinSize(map, checkedMap, row-1, col)
+        if col < len(map[0])-1 and not checkedMap[row][col+1]:
+            basinSize += getBasinSize(map, checkedMap, row, col+1)
+        if row < len(map)-1 and not checkedMap[row+1][col]:
+            basinSize += getBasinSize(map, checkedMap, row+1, col)
+        if col > 0 and not checkedMap[row][col-1]:
+            basinSize += getBasinSize(map, checkedMap, row, col-1)
+
+        return basinSize
+
+# Puzzle 2 solver
 def puzzle2(filename):
     # Read file
     input = input_parser(filename)
 
-    return 0
+    # Find all the lowpoints
+    lowPoints = []
+    nRows = len(input)
+    nCols = len(input[0])
+    for row in range(nRows):
+        for col in range(nCols):
+            adjacent = [10]*4
+            if not row == 0:
+                adjacent[0] = input[row-1][col]
+            if not col == nCols-1:
+                adjacent[1] = input[row][col+1]
+            if not row == nRows-1:
+                adjacent[2] = input[row+1][col]
+            if not col == 0:
+                adjacent[3] = input[row][col-1]
+
+            if input[row][col] < min(adjacent):
+                lowPoints.append((row, col))
+
+    # Find the size of each basin
+    basinSizes = [0]*len(lowPoints)
+    for i in range(len(lowPoints)):
+        basinSizes[i] = getBasinSize(input, [[False]*len(input[0]) for _ in range(len(input))], lowPoints[i][0], lowPoints[i][1])
+
+    # Sort the list and multipluy the last 3 elements
+    basinSizes.sort()
+    return basinSizes[-1]*basinSizes[-2]*basinSizes[-3]
 
 # Run tests for puzzle 1
 puzzle1TestPass = puzzle1('example1') == 15
@@ -60,7 +107,7 @@ if(puzzle1TestPass):
     print('Solution for puzzle 1: ' + str(puzzle1('input')))
 
 # Run tests for puzzle 2
-puzzle2TestPass = puzzle2('example1') == 2
+puzzle2TestPass = puzzle2('example1') == 1134
 if(puzzle2TestPass):
     print(colored('Tests for puzzle 2 PASS', 'green'))
 else:
