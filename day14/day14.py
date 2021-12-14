f447a691b5923c44c1cdce9fb690c3cd30b6bcb1
WPBack <daniel.a.back@telia.com>
2021-12-14 20:28:40 +0100

    Day 14 part 2 now multithreaded

--- day14/day14.py
+++ day14/day14.py
@@ -2,6 +2,7 @@
 import os
 from termcolor import colored
 from collections import Counter
+import threading
 
 # Input parser
 def input_parser(filename):
@@ -41,22 +42,50 @@ def puzzle1(filename):
     return numOccurances[0][1] - numOccurances[-1][1]
 
 # Puzzle 2
+def applyRules(polymer, rules):
+    oldPolymer = polymer.copy()
+    polymerIdx = 0
+    for oldPlymerIdx in range(len(oldPolymer)-1):
+        twoChars = ''.join(oldPolymer[oldPlymerIdx:oldPlymerIdx+2])
+        if twoChars in [rule[0] for rule in rules]:
+            ruleIdx = [rule[0] for rule in rules].index(twoChars)
+            polymer.insert(polymerIdx+1, rules[ruleIdx][1])
+            polymerIdx += 2
+        else:
+            polymerIdx += 1
+
+# Puzzle 2 solver
 def puzzle2(filename):
     # Read file
     (polymer, rules) = input_parser(filename)
 
     # Apply the rules 40 times
     for i in range(40):
-        oldPolymer = polymer.copy()
-        polymerIdx = 0
-        for oldPlymerIdx in range(len(oldPolymer)-1):
-            twoChars = ''.join(oldPolymer[oldPlymerIdx:oldPlymerIdx+2])
+        # Split the polymer into up to 12 parts for up to 12 threads
+        if len(polymer) < 12:
+            numThreads = len(polymer)
+        else:
+            numThreads = 12
+        chunkSize = int(len(polymer)/numThreads)
+        polymerChunks = [polymer[j:j+chunkSize].copy() for j in range(0, len(polymer), chunkSize)]
+
+        # Process the chunks
+        threads = []
+        for j in range(len(polymerChunks)):
+            threads.append(threading.Thread(target=applyRules, args=(polymerChunks[j], rules)))
+            threads[j].start()
+
+        # Wait for the threads to finnish and combine them
+        threads[0].join()
+        polymer = polymerChunks[0].copy()
+        for j in range(1,len(polymerChunks)):
+            threads[j].join()
+            twoChars = polymer[-1] + polymerChunks[j][0]
             if twoChars in [rule[0] for rule in rules]:
                 ruleIdx = [rule[0] for rule in rules].index(twoChars)
-                polymer.insert(polymerIdx+1, rules[ruleIdx][1])
-                polymerIdx += 2
-            else:
-                polymerIdx += 1
+                polymer.append(rules[ruleIdx][1])
+            polymer.extend(polymerChunks[j])
+
 
         print(str(i))
 
@@ -76,7 +105,7 @@ if(puzzle1TestPass):
     print('Solution for puzzle 1: ' + str(puzzle1('input')))
 
 # Run tests for puzzle 2
-puzzle2TestPass = puzzle2('example1') == 2188189693529
+puzzle2TestPass = puzzle2('example1') == 1588
 if(puzzle2TestPass):
     print(colored('Tests for puzzle 2 PASS', 'green'))
 else:
