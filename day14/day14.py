7f1a9ba4ba5fc64e6985733a433eb3aabfd6de0e
WPBack <daniel.a.back@telia.com>
2021-12-14 19:41:13 +0100

    Probably solved day 14 part 2 (takes long to run)

--- day14/day14.py
+++ day14/day14.py
@@ -43,9 +43,26 @@ def puzzle1(filename):
 # Puzzle 2
 def puzzle2(filename):
     # Read file
-    input = input_parser(filename)
+    (polymer, rules) = input_parser(filename)
 
-    return 0
+    # Apply the rules 40 times
+    for i in range(40):
+        oldPolymer = polymer.copy()
+        polymerIdx = 0
+        for oldPlymerIdx in range(len(oldPolymer)-1):
+            twoChars = ''.join(oldPolymer[oldPlymerIdx:oldPlymerIdx+2])
+            if twoChars in [rule[0] for rule in rules]:
+                ruleIdx = [rule[0] for rule in rules].index(twoChars)
+                polymer.insert(polymerIdx+1, rules[ruleIdx][1])
+                polymerIdx += 2
+            else:
+                polymerIdx += 1
+
+        print(str(i))
+
+    # Count the number of occurances
+    numOccurances = Counter(polymer).most_common()
+    return numOccurances[0][1] - numOccurances[-1][1]
 
 # Run tests for puzzle 1
 puzzle1TestPass = puzzle1('example1') == 1588
@@ -59,7 +76,7 @@ if(puzzle1TestPass):
     print('Solution for puzzle 1: ' + str(puzzle1('input')))
 
 # Run tests for puzzle 2
-puzzle2TestPass = puzzle2('example1') == 2
+puzzle2TestPass = puzzle2('example1') == 2188189693529
 if(puzzle2TestPass):
     print(colored('Tests for puzzle 2 PASS', 'green'))
 else:
